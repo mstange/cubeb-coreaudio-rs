721c3a9fa3fb2f2ded2e75f2532433094497d063
Chun-Min Chang <chun.m.chang@gmail.com>
2020-05-16 09:18:49 -0700

    Run tests that can cause backtrace -problems only

--- lldb.sh
+++ lldb.sh
@@ -3,13 +3,8 @@ cargo test --no-run
 test_exe=$(find target/debug/deps -perm +111 -type f)
 echo "Run test: $test_exe"
 
-# Regular tests
-lldb --batch --one-line run --source-on-crash dump.txt $test_exe
-
-# General aggregate tests
-agrs="test_aggregate --ignored --test-threads=1"
+agrs=test_aggregate_activate_clock_drift_compensation_for_an_unknown_aggregate_device
 lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
 
-# A specific one aggregate test
 agrs="test_aggregate_activate_clock_drift_compensation_for_a_blank_aggregate_device --ignored --test-threads=1"
 lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
