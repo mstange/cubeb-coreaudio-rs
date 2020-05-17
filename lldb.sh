314f72ecdae55c5553bd1628bca5357085c78d2f
Chun-Min Chang <chun.m.chang@gmail.com>
2020-05-17 08:31:22 -0700

    Run dummy test only

--- lldb.sh
+++ lldb.sh
@@ -6,8 +6,8 @@ echo "Run test: $test_exe"
 agrs=test_dummy
 lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
 
-agrs=test_aggregate_activate_clock_drift_compensation_for_an_unknown_aggregate_device
-lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
+# agrs=test_aggregate_activate_clock_drift_compensation_for_an_unknown_aggregate_device
+# lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
 
-agrs="test_aggregate_activate_clock_drift_compensation_for_a_blank_aggregate_device --ignored --test-threads=1"
-lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
+# agrs="test_aggregate_activate_clock_drift_compensation_for_a_blank_aggregate_device --ignored --test-threads=1"
+# lldb --batch --one-line run --source-on-crash dump.txt $test_exe -- $agrs
